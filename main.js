3c1d59fe46d97ed521e5f9856b44afdc7250a608
jzimme <jaz.uem@gmail.com>
2021-09-12 22:42:11 -0300

    README.md

---
+++ main.js
@@ -0,0 +1,115 @@
+function trocar() {
+    document.getElementsByClassName("mousemove");
+    //alert("trocar texto");
+}
+
+/*
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";;
+    //console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar() {
+    //window.open("https://globo.com");
+    window.location.href = "https://globo.com";
+}
+*/
+
+/*
+function clicou() {
+    alert("Obrigado por clicar");
+}
+*/
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar = true;
+    if(idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+*/
+
+/*
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+alert(soma(5, 10));
+*/
+
+/*
+var d = new Date();
+alert(d.getMonth() + 1); /*Sempre +1 porque começa do 0*/
+//alert(d.getMinutes()); 
+
+
+/*
+var count;
+for(count=0; count <=5; count++) {
+alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count ++;
+    //count = count + 1;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+//var idade = 18;
+if (idade >= 18) {
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
+console.log(frutas[1].nome);
+console.log(frutas[0].cor);
+*/
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta.nome);
+console.log(fruta.cor);
+*/
+
+//var lista = ["maçã"," pera", " laranja"]
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista[1]);
+//console.log(lista.join(" - "));
+//alert(lista[1]);
+
+//var nome = "José Augusto";
+//var idade = 29;
+//var idade2 = 15;
+//var frase = "Japão é o melhor time do mundo"
+/*alert("Bem vindo, " + nome + ". " + "Você tem " + idade + " anos.");*/
+//alert(idade + idade2);
+//console.log(frase.replace("Japão","Brasil"));
+//alert(frase.replace("do mundo","da América"));
+//alert(frase.replace("Japão","Brasil"));
